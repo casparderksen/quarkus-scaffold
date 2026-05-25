535d8f3f0b17ffc70c2e6c63a4daf74197312549
Caspar Derksen <caspard@me.com>
2026-05-25 17:08:29 +0200

    added generic RestApplication

---
+++ src/main/java/org/example/shared/infrastructure/adapter/in/rest/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Shared REST controllers and HTTP entrypoints.
+ *
+ * <p>Naming convention: {@code <Resource>Resource}. Example: {@code OrderResource}.
+ */
+package org.example.shared.infrastructure.adapter.in.rest;
